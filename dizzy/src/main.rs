53571fc171abd45483fd1d808f27d5f48a1e7801
RubikMH <mohamadhasans152@gmail.com>
2024-07-24 10:24:58 +0330

    1001th prime number for test toolols

--- dizzy/src/main.rs
+++ dizzy/src/main.rs
@@ -1,5 +1,32 @@
 #![deny(clippy::all)]
 
 fn main() {
-    println!("it has changed by Rubik");
+    let mut counter = 0;
+    let mut number = 2;
+    let result = loop {
+        let mut prime_number = 0;
+        let mut is_prime: bool = true;
+
+        for num in 2..number {
+            if is_prime {
+                if number % num == 0 {
+                    is_prime = false;
+                }
+            } else {
+                break;
+            }
+        }
+        // if is true
+        if is_prime {
+            counter += 1;
+            prime_number = number;
+            println!("counter : {:#?} , number is : {:#?}", counter, prime_number)
+        }
+
+        if counter == 1001 {
+            break prime_number;
+        }
+        number += 1;
+    };
+    println!(" result = {:?}", result);
 }
