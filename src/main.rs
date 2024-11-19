4fedcfed992c97d7503423d09d5d2a825aef74d9
RubikMH <mohamadhasans152@gmail.com>
2024-11-19 18:14:02 +0330

    read docker socket

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
 #![deny(clippy::all)]
 
-fn main() {
-    println!("hi")
+use std::{io::Read, os::unix::net::UnixListener};
+
+fn main() -> std::io::Result<()> {
+    println!("service is running ...");
+    let listener = UnixListener::bind("/var/run/docker.sock")?;
+
+    match listener.accept() {
+        Ok((mut socket, addr)) => {
+            println!("Got a client: {:?} - {:?}", socket, addr);
+            let mut response = String::new();
+            socket.read_to_string(&mut response)?;
+            println!("{}", response);
+        }
+        Err(e) => println!("accept function failed: {:?}", e),
+    }
+
+    Ok(())
 }
